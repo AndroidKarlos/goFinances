46ae9984c23c09cfeb168a5a5542752ac3178ed9
Jose Carlos Bezerra <carlos.bezerradasilva@hotmail.com>
2022-01-05 07:12:18 -0300

    finalizando listagem

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useFocusEffect } from '@react-navigation/native';
 import { HighlightCard } from '../../components/HighlighCard';
 import { TransactionsCard, TransactionCardProps } from '../../components/TransactionsCard';
 
@@ -65,6 +66,10 @@ export function Dashboard() {
     useEffect(() => {
         loadTransactions();
     }, []);
+
+    useFocusEffect(useCallback(() =>{
+        loadTransactions();
+    }, []))
 
     return (
         <Container>
